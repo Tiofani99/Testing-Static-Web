9488c463e8782956c6ea92f73f4e2128b35af7cd
Tiofani99 <tiofani03@gmail.com>
2020-04-14 20:44:43 +0700

    Sekarang functional test bisa input nilai

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -12,8 +12,6 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        # Edith has heard about a cool new online to-do app. She goes
-        # to check out its homepage
         self.browser.get('http://localhost:8000')
 
         # She notices the page title and header mention to-do lists
